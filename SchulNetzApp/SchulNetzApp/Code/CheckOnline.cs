40ddf9a91ea2d2475b4a223a377ae5b169d67d00
Joshua De Marco <joshua.dm@hispeed.ch>
2019-04-28 21:55:58 +0200

    LoginCheck initialized (header for check missing)

--- SchulNetzApp/SchulNetzApp/Code/CheckOnline.cs
+++ SchulNetzApp/SchulNetzApp/Code/CheckOnline.cs
@@ -1,34 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Net.Http;
-using System.Net;
-using System.Threading.Tasks;
 using System.Diagnostics;
+using System.Net.Http;
+using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace SchulNetzApp.Code
 {
     public class CheckOnline
     {
-        //Vars container?
 
-            
 
-        public static async Task checkonline(User user) {
+
+        public static async Task<bool> checkonline(User usercred)
+        {
             string url = "https://www.schul-netz.com/unterstrass/loginto.php?pageid=21311";
 
 
-            using (HttpClient client = new HttpClient()) {
-
+            using (HttpClient client = new HttpClient())
+            {
                 try
                 {
-                    
+
                     string responseBody = await client.GetStringAsync(url);
 
-                    Console.WriteLine(responseBody);
 
-                }catch (HttpRequestException e){throw new ApplicationException("An Error occured while connecting");}
+
+                    Device.BeginInvokeOnMainThread(() => { Debug.WriteLine(responseBody); });
+                    return true;
+
+                }
+                catch (HttpRequestException e) { Device.BeginInvokeOnMainThread(() => { Debug.WriteLine("HttpClient Returned Exception: {0})", e.Message); }); return false; } //IF HttpClient Fails
 
             }
 
--- SchulNetzApp/SchulNetzApp/Login.xaml.cs
+++ SchulNetzApp/SchulNetzApp/Login.xaml.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
-using System.Net;
-using System.Web;
-using System.IO;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -21,42 +15,55 @@ namespace SchulNetzApp
 
 
 
-        private async void Login_click(object sender, EventArgs args)
+
+
+        public async void Login_click(object sender, EventArgs args)
         {
 
-            if (string.IsNullOrEmpty(UserInput.Text) || string.IsNullOrEmpty(PassInput.Text)) //<--- not wworking
+            if (string.IsNullOrEmpty(UserInput.Text) || string.IsNullOrEmpty(PassInput.Text))
             {
                 throw new ApplicationException("Nothing Filled in");
-            }else{
 
-                var usercred = new User // nicht public
+            }
+            else
+            {
+
+                var usercred = new User // nicht public??
                 {
                     Username = UserInput.Text,
                     Password = PassInput.Text
                 };
 
                 var isValid = AreCredOk(usercred);
-                if (isValid) {
+                if (await isValid)
+                {
+
+
                     App.IsUserLoggedIn = true;
-                    Navigation.PushAsync() // Navigate new Main as Mainpage;
+
+                    // Navigate new Main as Mainpage
+                    await Navigation.PushModalAsync(new MainPage());
+
                     //Do Tick Animation
                 }
                 else
-                { 
+                {
                     //Make Cross Animation
                 }
-            
 
-                }
 
             }
 
-        
-        public bool AreCredOk(User user) {
-            return true; //CheckOnline Referrenz
         }
 
 
+
+
+        public async Task<bool> AreCredOk(User usercred)
+        {
+            return await Code.CheckOnline.checkonline(usercred); //CheckOnline-Refferenz DIDIT!
         }
 
     }
+}
+
