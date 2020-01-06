1626cd4bf06fab94a23779b961c428847f2fb82f
Joshua De Marco <joshua.dm@hispeed.ch>
2020-01-06 16:18:43 +0100

    Added iOS Auth from old file

---
+++ SchulNetzApp/SchulNetzApp.iOS/Code/FirebaseIOSAuth.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Foundation;
+using UIKit;
+
+namespace SchulNetzApp.iOS.Code
+{
+    class FirebaseIOSAuth
+    {
+       
+
+    [Register("AppDelegate")]
+
+        public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+
+        {
+
+            public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+
+            {
+
+                global::Xamarin.Forms.Forms.Init();
+
+                LoadApplication(new App());
+
+
+
+                Firebase.Core.App.Configure();
+
+                return base.FinishedLaunching(app, options);
+
+            }
+        }
+    } 
+}
